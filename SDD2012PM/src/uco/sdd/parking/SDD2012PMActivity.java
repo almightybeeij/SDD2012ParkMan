eae7a8dbda6d2439bccced7dcdcdb26340a3ad4b
Lee Tyler Carey <lee.tyler.carey@gmail.com>
2012-03-27 19:06:25 -0500

    dfg

---
+++ SDD2012PM/src/uco/sdd/parking/SDD2012PMActivity.java
@@ -0,0 +1,13 @@
+package uco.sdd.parking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SDD2012PMActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
