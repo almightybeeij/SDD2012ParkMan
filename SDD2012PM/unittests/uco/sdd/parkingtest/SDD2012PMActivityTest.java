3cde3fdde091f549a396e35673e6fb736b4d4657
Cory Wilson <wilson7880@gmail.com>
2012-03-29 09:50:27 -0500

    New Unit Test

---
+++ SDD2012PM/unittests/uco/sdd/parkingtest/SDD2012PMActivityTest.java
@@ -0,0 +1,14 @@
+package uco.sdd.parkingtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SDD2012PMActivityTest {
+
+	@Test
+	public void testOnCreateBundle() {
+		fail("Not yet implemented");
+	}
+
+}
