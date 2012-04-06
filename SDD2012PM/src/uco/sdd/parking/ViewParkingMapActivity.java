1d0a694d24f1e4fb1c3190157cec87855841f98f
Cory Wilson <wilson7880@gmail.com>
2012-04-06 14:28:32 -0500

    Added ViewParkingMapActivity

---
+++ SDD2012PM/src/uco/sdd/parking/ViewParkingMapActivity.java
@@ -0,0 +1,22 @@
+package uco.sdd.parking;
+
+import com.google.android.maps.MapActivity;
+import android.os.Bundle;
+
+public class ViewParkingMapActivity extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
