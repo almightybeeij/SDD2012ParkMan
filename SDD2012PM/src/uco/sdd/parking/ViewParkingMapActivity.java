a937ace5a4a94910ee5fb2bc259db7da476834d2
Cory Wilson <wilson7880@gmail.com>
2012-04-06 14:33:41 -0500

    Added MapView to activity

--- SDD2012PM/src/uco/sdd/parking/ViewParkingMapActivity.java
+++ SDD2012PM/src/uco/sdd/parking/ViewParkingMapActivity.java
@@ -1,22 +1,46 @@
 package uco.sdd.parking;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 
 public class ViewParkingMapActivity extends MapActivity {
 
+	private MapView mapView;
+	private MapController mc;
+	
 	@Override
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		
 	    super.onCreate(savedInstanceState);
 	
-	    // TODO Auto-generated method stub
+	    mapView = (MapView)findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        
+	    mc = mapView.getController();
+        String coordinates[] = {"35.654108", "-97.473863"};
+        double lat = Double.parseDouble(coordinates[0]);
+        double lng = Double.parseDouble(coordinates[1]);
+ 
+        GeoPoint p = new GeoPoint(
+            (int) (lat * 1E6), 
+            (int) (lng * 1E6));
+ 
+        mc.animateTo(p);
+        mc.setZoom(19); 
+        
+        mapView.setStreetView(true);
+		
+        mapView.invalidate();
 	}
 
 }
