d43ba7bb095dfc7090d54ff2749b72bf6c387693
Cory Wilson <wilson7880@gmail.com>
2012-04-10 17:15:50 -0500

    Update

--- SDD2012PM/src/uco/sdd/parking/SearchActivity.java
+++ SDD2012PM/src/uco/sdd/parking/SearchActivity.java
@@ -11,6 +11,8 @@ import uco.sdd.utility.*;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -29,7 +31,7 @@ public class SearchActivity extends Activity {
 	    parkingLots = new ArrayList<String>();
 	    
 	    HTTPDataAccess dac = new HTTPDataAccess(this,
-    			getString(R.string.url_select), new SearchJSONListener());
+    			getString(R.string.url_select), new SearchBuildingJSONListener());
 	    
 	    dac.setStatement(getString(R.string.search_smt_building));
 	    dac.setTypes(getString(R.string.search_smt_building_types));
@@ -42,7 +44,7 @@ public class SearchActivity extends Activity {
 		
 	}
 	
-	private class SearchJSONListener implements GetJSONListener
+	private class SearchBuildingJSONListener implements GetJSONListener
 	{
 		public void onRemoteCallComplete(JSONArray jArray) {
 			    	
@@ -62,7 +64,7 @@ public class SearchActivity extends Activity {
 				    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
 				    			R.layout.search_spinner_item, R.id.search_spn_itemtext, buildings);
 				    	Spinner spinnerBuildings = (Spinner)findViewById(R.id.search_spn_building);
-				    	
+				    	spinnerBuildings.setOnItemSelectedListener(new MyOnItemSelectedListener());
 				    	spinnerBuildings.setAdapter(adapter);
 	    			}
 	    		}
@@ -71,5 +73,59 @@ public class SearchActivity extends Activity {
 	    		e.printStackTrace();
 	    	}
 		}
+	}
+	
+	private class SearchParkingLotJSONListener implements GetJSONListener
+	{
+		public void onRemoteCallComplete(JSONArray jArray) {
+			    	
+	    	try
+	    	{
+	    		if (jArray != null)
+	    		{
+	    			if (jArray.length() > 0)
+	    			{
+				    	for(int index = 0; index < jArray.length(); index++)
+				    	{
+				    		JSONObject json_data = jArray.getJSONObject(index);
+				    		
+				    		parkingLots.add(json_data.getString("name"));
+				    	}
+				    	
+				    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+				    			R.layout.search_spinner_item, R.id.search_spn_itemtext, parkingLots);
+				    	
+				    	Spinner spinnerBuildings = (Spinner)findViewById(R.id.search_spn_building);
+				    	spinnerBuildings.setOnItemSelectedListener(new MyOnItemSelectedListener());
+				    	spinnerBuildings.setAdapter(adapter);
+	    			}
+	    		}
+	    	}
+	    	catch (JSONException e)	{
+	    		e.printStackTrace();
+	    	}
+		}
+	}
+	
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+	    public void onItemSelected(AdapterView<?> parent,
+	        View view, int pos, long id)
+	    {
+	    	String buildingValue = parent.getItemAtPosition(pos).toString();
+	    	
+	    	HTTPDataAccess dac = new HTTPDataAccess(getApplicationContext(),
+	    			getString(R.string.url_select), new SearchParkingLotJSONListener());
+		    
+		    dac.setStatement(getString(R.string.search_smt_parkinglots));
+		    dac.setTypes(getString(R.string.search_smt_parkinglots_types));
+	    	dac.addNewBindVariable("building", buildingValue, false);
+	    	
+	    	dac.executeSelect();
+	    }
+
+	    public void onNothingSelected(AdapterView<?> parent) {
+	      // Do nothing.
+	    }
 	}
 }
