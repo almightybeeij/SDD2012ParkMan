359d890a643008995d52bc1d78927ce7bca16157
Cory Wilson <wilson7880@gmail.com>
2012-04-10 00:14:40 -0500

    Added spinner controls to SearchActivity

--- SDD2012PM/src/uco/sdd/parking/SearchActivity.java
+++ SDD2012PM/src/uco/sdd/parking/SearchActivity.java
@@ -1,19 +1,75 @@
 package uco.sdd.parking;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import uco.sdd.utility.*;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchActivity extends Activity {
 
+	private ArrayList<String> buildings;
+	private ArrayList<String> parkingLots;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.search_layout);
+	    
+	    buildings = new ArrayList<String>();
+	    parkingLots = new ArrayList<String>();
+	    
+	    HTTPDataAccess dac = new HTTPDataAccess(this,
+    			getString(R.string.url_select), new SearchJSONListener());
+	    
+	    dac.setStatement(getString(R.string.search_smt_building));
+	    dac.setTypes(getString(R.string.search_smt_building_types));
+    	dac.addNewBindVariable("One", "1", false);
+    	
+    	dac.executeSelect();
 	}
 
 	public void searchOnClick(View view) {
 		
+	}
+	
+	private class SearchJSONListener implements GetJSONListener
+	{
+		public void onRemoteCallComplete(JSONArray jArray) {
+			    	
+	    	try
+	    	{
+	    		if (jArray != null)
+	    		{
+	    			if (jArray.length() > 0)
+	    			{
+				    	for(int index = 0; index < jArray.length(); index++)
+				    	{
+				    		JSONObject json_data = jArray.getJSONObject(index);
+				    		
+				    		buildings.add(json_data.getString("name"));
+				    	}
+				    	
+				    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+				    			R.layout.search_spinner_item, R.id.search_spn_itemtext, buildings);
+				    	Spinner spinnerBuildings = (Spinner)findViewById(R.id.search_spn_building);
+				    	
+				    	spinnerBuildings.setAdapter(adapter);
+	    			}
+	    		}
+	    	}
+	    	catch (JSONException e)	{
+	    		e.printStackTrace();
+	    	}
+		}
 	}
 }
