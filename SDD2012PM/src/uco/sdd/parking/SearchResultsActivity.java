e092ccbd73a20d96f4c6b152ccc2ce8a4f519578
Cory Wilson <wilson7880@gmail.com>
2012-04-12 09:16:31 -0500

    Added SearchResultsActivity; searchresults_layout; searchresults_values

---
+++ SDD2012PM/src/uco/sdd/parking/SearchResultsActivity.java
@@ -0,0 +1,15 @@
+package uco.sdd.parking;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends ListActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.searchresults_layout);
+	}
+
+}
