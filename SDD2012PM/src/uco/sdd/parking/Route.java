818e0caa281aad022e013c721f35dab4ab7feb8d
Cory Wilson <wilson7880@gmail.com>
2012-04-17 00:05:12 -0500

    Forgot to add new Route and RouteStep classes

---
+++ SDD2012PM/src/uco/sdd/parking/Route.java
@@ -0,0 +1,97 @@
+package uco.sdd.parking;
+
+import java.util.ArrayList;
+
+import com.google.android.maps.GeoPoint;
+
+public class Route {
+
+	private GeoPoint boundsNE;
+	private GeoPoint boundsSW;
+	private GeoPoint startLocation;
+	private GeoPoint endLocation;
+	
+	private String distance;
+	private String duration;
+	private String startAddress;
+	private String endAddress;
+	
+	private ArrayList<RouteStep> steps;
+	
+	public Route()
+	{
+		steps = new ArrayList<RouteStep>();
+	}
+
+	public GeoPoint getBoundsNE() {
+		return boundsNE;
+	}
+
+	public void setBoundsNE(GeoPoint boundsNE) {
+		this.boundsNE = boundsNE;
+	}
+
+	public GeoPoint getBoundsSW() {
+		return boundsSW;
+	}
+
+	public void setBoundsSW(GeoPoint boundsSW) {
+		this.boundsSW = boundsSW;
+	}
+
+	public GeoPoint getStartLocation() {
+		return startLocation;
+	}
+
+	public void setStartLocation(GeoPoint startLocation) {
+		this.startLocation = startLocation;
+	}
+
+	public GeoPoint getEndLocation() {
+		return endLocation;
+	}
+
+	public void setEndLocation(GeoPoint endLocation) {
+		this.endLocation = endLocation;
+	}
+
+	public String getDistance() {
+		return distance;
+	}
+
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getStartAddress() {
+		return startAddress;
+	}
+
+	public void setStartAddress(String startAddress) {
+		this.startAddress = startAddress;
+	}
+
+	public String getEndAddress() {
+		return endAddress;
+	}
+
+	public void setEndAddress(String endAddress) {
+		this.endAddress = endAddress;
+	}
+
+	public ArrayList<RouteStep> getSteps() {
+		return steps;
+	}
+
+	public void setSteps(ArrayList<RouteStep> steps) {
+		this.steps = steps;
+	}
+}
---
+++ SDD2012PM/src/uco/sdd/parking/RouteStep.java
@@ -0,0 +1,78 @@
+package uco.sdd.parking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+
+public class RouteStep {
+
+	private String instructions;
+	private String distance;
+	private String duration;
+	private String travelMode;
+	
+	private List<GeoPoint> polyPoints;
+	private GeoPoint startLocation;
+	private GeoPoint endLocation;
+	
+	public RouteStep() {
+		polyPoints = new ArrayList<GeoPoint>();
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	public String getDistance() {
+		return distance;
+	}
+
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getTravelMode() {
+		return travelMode;
+	}
+
+	public void setTravelMode(String travelMode) {
+		this.travelMode = travelMode;
+	}
+
+	public List<GeoPoint> getPolyPoints() {
+		return polyPoints;
+	}
+
+	public void setPolyPoints(List<GeoPoint> polyPoints) {
+		this.polyPoints = polyPoints;
+	}
+
+	public GeoPoint getStartLocation() {
+		return startLocation;
+	}
+
+	public void setStartLocation(GeoPoint startLocation) {
+		this.startLocation = startLocation;
+	}
+
+	public GeoPoint getEndLocation() {
+		return endLocation;
+	}
+
+	public void setEndLocation(GeoPoint endLocation) {
+		this.endLocation = endLocation;
+	}
+}
