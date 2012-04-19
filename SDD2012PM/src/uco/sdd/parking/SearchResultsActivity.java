3ab436be81c77bd6bd692bc43dec26500b89bba5
Cory Wilson <wilson7880@gmail.com>
2012-04-18 22:56:47 -0500

    Removed unused import in SearchResultsActivity

--- SDD2012PM/src/uco/sdd/parking/SearchResultsActivity.java
+++ SDD2012PM/src/uco/sdd/parking/SearchResultsActivity.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
